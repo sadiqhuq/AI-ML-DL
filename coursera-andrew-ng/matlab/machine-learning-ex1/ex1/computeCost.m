787144a0b8a4397b9ba2c441c49e764a44a7cf83
Sadiq Huq <sadiqhuq@gmail.com>
2018-03-04 17:32:50 +0100

    ex1 vectorized computeCost

--- coursera-andrew-ng/matlab/machine-learning-ex1/ex1/computeCost.m
+++ coursera-andrew-ng/matlab/machine-learning-ex1/ex1/computeCost.m
@@ -13,7 +13,22 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% for i = 1:m
+%     J += (theta(1,1) +theta(2,1)*X(i,2)-  y(i))^2;
+% end
 
+%% disp( X)
+%% Implementation Note
+%% We store each example as a row in the the X
+%% matrix in Octave/MATLAB. To take into account the intercept term (θ 0 ),
+%% we add an additional first column to X and set it to all ones. This allows
+%% us to treat θ 0 as simply another ‘feature’.
+%% Column 1 of X is always 1, helps in coding vectorized form
+
+J = sum(((theta' * X')' - y) .^ 2);
+
+
+J = J / (2 * m);
 
 
 
