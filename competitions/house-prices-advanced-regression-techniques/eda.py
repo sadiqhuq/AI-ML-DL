6e690f3e5439b5e43ba82c8780a7dace042fe167
Sadiq Huq <sadiqhuq@gmail.com>
2018-04-01 13:54:35 +0200

    eda heatmap

--- competitions/house-prices-advanced-regression-techniques/eda.py
+++ competitions/house-prices-advanced-regression-techniques/eda.py
@@ -2,7 +2,7 @@
 import numpy  as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
+import seaborn as sns
 
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
@@ -14,13 +14,21 @@ train = pd.read_csv('./input/train.csv')
 
 # print('Features: \n', *list(train), sep='\t')
 
-print ( 'Number of features:  ', train.shape[1] )
-print ( 'Number of instances: ', train.shape[0] )
+print ( 'Train - number of features:  ', train.shape[1] )
+print ( 'Train - number of instances: ', train.shape[0] )
 
 # # Data Check
 # print ( train.head() )
 # print ( train.describe() )
 
+# # Use Correlation to identify 5 important features
+
+train_corr = train.corr()
+train_corr.sort_values(["SalePrice"], ascending = False, inplace = True)
+print(train_corr.SalePrice[train_corr.SalePrice>0.5])
+fig, ax = plt.subplots(figsize=(15,15))
+sns.heatmap(train_corr, vmin=-1.0,vmax=1.0, square=True,cmap='RdBu',center=0);
+
+
+
 # # Use PCA to identify 5 important features
-
-
